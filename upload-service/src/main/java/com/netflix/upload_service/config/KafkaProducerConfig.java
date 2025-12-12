1127c654679cb81a752c352c8564a216cd0c7d80
neel0086 <neelmehta0086@gmail.com>
2025-12-12 08:46:41 +0530

    worflows for docker automation

--- upload-service/src/main/java/com/netflix/upload_service/config/KafkaProducerConfig.java
+++ upload-service/src/main/java/com/netflix/upload_service/config/KafkaProducerConfig.java
@@ -1,6 +1,5 @@
 package com.netflix.upload_service.config;
 
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
